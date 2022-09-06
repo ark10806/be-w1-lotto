8f74d3a84d939044056e2094de67c1b4b3aa805b
PPPosik <eoanswkgksk@gmail.com>
2022-09-06 16:55:59 +0900

    인덴트 통일

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -16,8 +16,8 @@ public class Lotto {
 
     public int getHitRate(List<Integer> hit) {
         int hitRate = 0;
-        for (int i = 0 ; i < 6 ; i ++){
-            hitRate = numbers.contains(hit.get(i))? hitRate + 1 : hitRate;
+        for (int i = 0; i < 6; i++) {
+            hitRate = numbers.contains(hit.get(i)) ? hitRate + 1 : hitRate;
         }
         return hitRate;
     }
