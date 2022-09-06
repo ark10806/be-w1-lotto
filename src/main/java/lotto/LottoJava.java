aea7eb45c4a9c765b04ee738174194794f6a1303
Egg <71698417+LEEAHRI@users.noreply.github.com>
2022-09-06 18:20:23 +0900

    Revert "Revert "Egg eigen""

--- src/main/java/lotto/LottoJava.java
+++ src/main/java/lotto/LottoJava.java
@@ -1,14 +1,52 @@
 package lotto;
-
-
+import java.lang.reflect.Array;
+import java.util.Iterator;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
+
 
 public class LottoJava {
-    private int coin;
+    final int LOTTONUM = 6;
+    final int LOTTOMAX = 45;
+    final Random rand = new Random();
+    private int coin, count;
+    private ArrayList<ArrayList<Integer>> papers = new ArrayList<>();
 
-    public void insertCoin(){
+    public void insertCoin() {
         Scanner sc = new Scanner(System.in);
         System.out.println("구입금액을 입력해 주세요.");
         coin = sc.nextInt();
+    }
+    public void lottoCount() {
+        count = coin / 1000;
+        System.out.println("로또의 개수는 :" + count);
+    }
+
+    private ArrayList getPaper() {
+        ArrayList<Integer> paper = new ArrayList<>();
+        for(int i=0; i<LOTTONUM; i++){
+            paper.add(rand.nextInt(LOTTOMAX)+1);
+        }
+        return paper;
+    }
+
+    public void getAllPapers() {
+        papers.clear();
+        for(int i=0; i<count; i++) {
+            papers.add(getPaper());
+        }
+
+        Iterator iter = papers.iterator();
+        while(iter.hasNext()) {
+            System.out.println(iter.next());
+        }
+    }
+
+    public void run(){
+        insertCoin();
+        lottoCount();
+        getAllPapers();
     }
 }
