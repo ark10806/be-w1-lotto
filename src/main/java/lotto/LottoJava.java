5293791843440006b42eff8ac23a310042cdda7f
egg.boiled <egg.boiled@kakaocorp.com>
2022-09-06 17:28:27 +0900

    [egg] 로또 개수 생성

--- src/main/java/lotto/LottoJava.java
+++ src/main/java/lotto/LottoJava.java
@@ -12,9 +12,7 @@ public class LottoJava {
         System.out.println("구입금액을 입력해 주세요.");
         coin = sc.nextInt();
     }
-
     public void lottoCount() {
-
         count = coin / 1000;
         System.out.println("로또의 개수는 :" + count);
     }
