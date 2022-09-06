236513f8465447d3aade1ca506f0b9ed8f29dcf9
lyra-lee <lyra.lee@kakaocorp.com>
2022-09-06 14:54:06 +0900

    Lotto 자동 구매 main 프로세스 작성

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,11 +1,33 @@
 package lotto;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    public static String getHello() {
-        return "Hello";
+public class Main {
+    public static LottoGenerator lottoGenerator = new LottoAutoGenerator();
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("구입금액을 입력해 주세요.");
+        int cnt = (int) Math.floor(Integer.parseInt(br.readLine())/1000);
+        System.out.printf("%d개를 구매했습니다.\n", cnt);
+
+        // 랜덤 숫자 출력
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i = 0 ; i < cnt ; i++){
+            Lotto lotto = lottoGenerator.generate();
+            lottos.add(lotto);
+            System.out.println(lotto.toString());
+        }
+
+        // 지난 주 당첨 번호
+        System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+        List<String> hitsString = Arrays.asList(br.readLine().split(", "));
+        List<Integer> hits = new ArrayList<>();
+        for(String s : hitsString) hits.add(Integer.parseInt(s));
     }
-    public static void main(String[] args) {
-        System.out.println(Main.getHello());
-    }
+
 }
