01e92ed924c0352bbda51390e5136bf080d4018e
eigen-vector <eigen.vector@kakaocorp.com>
2022-09-07 13:08:26 +0900

    [Add] Award 클래스 구현: 당첨 여부판단

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -1,6 +1,9 @@
 package lotto;
 import lotto.LottoJava;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Main {
 
     public static String getHello() {
@@ -9,6 +12,11 @@ public class Main {
 
     public static void main(String[] args) {
         LottoJava lotto = new LottoJava();
-        lotto.run();
+        ArrayList<HashSet<Integer>> candidates = lotto.run();
+
+        Award award = new Award();
+        award.setWinnerNumbers();
+        award.compute(candidates);
+        award.showResult();
     }
 }
